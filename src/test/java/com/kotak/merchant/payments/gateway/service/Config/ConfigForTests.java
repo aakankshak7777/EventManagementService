262d1c92028091668a3075fc6235575f90711cfe
kunal.miahra <kunal.mishra@Kotak.com>
2024-06-18 15:44:11 +0530

    Commit Basic Classes

--- src/test/java/com/kotak/merchant/payments/gateway/service/Config/ConfigForTests.java
+++ src/test/java/com/kotak/merchant/payments/gateway/service/Config/ConfigForTests.java
@@ -1,7 +1,5 @@
 package com.kotak.merchant.payments.gateway.service.Config;
 
-import com.kotak.merchant.payments.gateway.service.Config.ContainerConfig;
-import com.kotak.merchant.payments.gateway.service.Config.SetupConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
