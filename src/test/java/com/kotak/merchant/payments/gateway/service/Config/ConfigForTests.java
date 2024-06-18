f56648e88db05098f42d95646c90deb56da51add
kunal.miahra <kunal.mishra@Kotak.com>
2024-06-18 15:43:12 +0530

    Commit Basic Classes

--- src/test/java/com/kotak/merchant/payments/gateway/service/Config/ConfigForTests.java
+++ src/test/java/com/kotak/merchant/payments/gateway/service/Config/ConfigForTests.java
@@ -1,10 +1,12 @@
-//package com.kotak.inventoryservice.Config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-//
-//@Configuration
-//@Import({ContainerConfig.class, SetupConfig.class})
-//public class ConfigForTests extends WebMvcConfigurationSupport {
-//}
+package com.kotak.merchant.payments.gateway.service.Config;
+
+import com.kotak.merchant.payments.gateway.service.Config.ContainerConfig;
+import com.kotak.merchant.payments.gateway.service.Config.SetupConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@Import({ContainerConfig.class, SetupConfig.class})
+public class ConfigForTests extends WebMvcConfigurationSupport {
+}
---
+++ src/test/java/com/kotak/merchant/payments/gateway/service/Config/SetupConfig.java
@@ -0,0 +1,4 @@
+package com.kotak.merchant.payments.gateway.service.Config;
+
+public class SetupConfig {
+}
