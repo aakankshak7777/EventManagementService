8492696c3da27f5a9603ca1a596647e074d40e88
kunal.miahra <kunal.mishra@Kotak.com>
2024-07-01 15:40:52 +0530

    Kunal / Aman Check in.

--- src/main/java/com/kmbl/eventmanagementservice/service/streams/serializers/CollectCallbackEventSerializer.java
+++ src/main/java/com/kmbl/eventmanagementservice/service/streams/serializers/CollectCallbackEventSerializer.java
@@ -1,9 +1,5 @@
 package com.kmbl.eventmanagementservice.service.streams.serializers;
 
-
 import com.kmbl.eventmanagementservice.service.dtos.CollectCallbackEvent;
 import com.kmbl.eventmanagementservice.utils.JsonSerializer;
-
-
-
 public class CollectCallbackEventSerializer extends JsonSerializer<CollectCallbackEvent> { }
